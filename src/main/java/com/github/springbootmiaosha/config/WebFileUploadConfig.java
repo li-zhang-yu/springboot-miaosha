5a8be7581b33657b3b92578b738c331fbc3b5262
lzy8339789 <1641189550@qq.com>
2019-08-13 00:49:09 +0800

    七牛云图片上传配置类。

--- src/main/java/com/github/springbootmiaosha/config/WebFileUploadConfig.java
+++ src/main/java/com/github/springbootmiaosha/config/WebFileUploadConfig.java
@@ -1,5 +1,10 @@
 package com.github.springbootmiaosha.config;
 
+import com.qiniu.common.Zone;
+import com.qiniu.storage.BucketManager;
+import com.qiniu.storage.UploadManager;
+import com.qiniu.util.Auth;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -52,6 +57,49 @@ public class WebFileUploadConfig {
         StandardServletMultipartResolver multipartResolver = new StandardServletMultipartResolver();
         multipartResolver.setResolveLazily(this.multipartProperties.isResolveLazily());
         return multipartResolver;
+    }
+
+    /**
+     * 华东机房
+     * @return
+     */
+    @Bean
+    public com.qiniu.storage.Configuration qiniuConfig(){
+        //构造一个带指定Zone对象的配置类
+        return new com.qiniu.storage.Configuration(Zone.zone0());
+    }
+
+    /**
+     * 构建一个七牛上传工具实例
+     * @return
+     */
+    @Bean
+    public UploadManager uploadManager(){
+        return new UploadManager(qiniuConfig());
+    }
+
+    @Value("${qiniu.AccessKey}")
+    private String accessKey;
+
+    @Value("${qiniu.SecretKey}")
+    private String secretKey;
+
+    /**
+     * 认证信息实例
+     * @return
+     */
+    @Bean
+    public Auth auth(){
+        return Auth.create(accessKey, secretKey);
+    }
+
+    /**
+     * 构建七牛空间管理实例
+     * @return
+     */
+    @Bean
+    public BucketManager bucketManager(){
+        return new BucketManager(auth(), qiniuConfig());
     }
 
 
