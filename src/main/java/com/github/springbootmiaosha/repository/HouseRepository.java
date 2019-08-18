996d53e34721676fdf9c94a8c8dd440934993415
lzy8339789 <1641189550@qq.com>
2019-08-18 11:37:50 +0800

    房屋接口继承类进行修改。

--- src/main/java/com/github/springbootmiaosha/repository/HouseRepository.java
+++ src/main/java/com/github/springbootmiaosha/repository/HouseRepository.java
@@ -1,7 +1,8 @@
 package com.github.springbootmiaosha.repository;
 
 import com.github.springbootmiaosha.entity.House;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 /**
  * 房屋相关接口
@@ -9,6 +10,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @author lizhangyu
  * @date 2019-08-16
  */
-public interface HouseRepository extends JpaRepository<House, Long> {
+public interface HouseRepository extends PagingAndSortingRepository<House, Long>, JpaSpecificationExecutor<House> {
 }
 
