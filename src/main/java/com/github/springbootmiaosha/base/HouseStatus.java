59ab3ed5086b2a72670982b92737f9c612a27160
lzy8339789 <1641189550@qq.com>
2019-08-18 10:54:47 +0800

    房源状态枚举类提交。

---
+++ src/main/java/com/github/springbootmiaosha/base/HouseStatus.java
@@ -0,0 +1,40 @@
+package com.github.springbootmiaosha.base;
+
+/**
+ * 房源状态
+ *
+ * @author lizhangyu
+ * @date 2019-08-17
+ */
+public enum HouseStatus {
+
+    /**
+     * 未审核
+     */
+    NOT_AUDITED(0),
+
+    /**
+     * 审核通过
+     */
+    PASSES(1),
+
+    /**
+     * 已出租
+     */
+    RENTED(2),
+
+    /**
+     * 逻辑删除
+     */
+    DELETED(3);
+
+    private int value;
+
+    HouseStatus(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
